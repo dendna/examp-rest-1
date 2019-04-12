7479f7cc4a6446994720243c115e0fc47c2214c4
Денис <@DESKTOP-07DNFR0>
2019-04-12 17:08:33 +0300

    add postgres

--- main.go
+++ main.go
@@ -1,33 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
 )
 
 // Book ...
 type Book struct {
-	ID     int    `json:id`
-	Title  string `json:title`
-	Author string `json: author`
-	Year   string `json:year`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
 }
 
 var books []Book
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	router := mux.NewRouter()
 
-	books = append(books, Book{ID: 1, Title: "Golang pointers", Author: "Mr.Golang", Year: "2010"},
-		Book{ID: 2, Title: "Goroutines", Author: "Mr.Goroutine", Year: "2011"},
-		Book{ID: 3, Title: "Golang routers", Author: "Mr.Golang", Year: "2012"},
-		Book{ID: 4, Title: "Golang concurrency", Author: "Mr.Currency", Year: "2013"},
-		Book{ID: 5, Title: "Golang good parts", Author: "Mr.Good", Year: "2014"},
-	)
+	pgURL, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	logFatal(err)
+
+	db, err = sql.Open("postgres", pgURL)
+	logFatal(err)
+
+	err = db.Ping()
+	logFatal(err)
+
+	router := mux.NewRouter()
 
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
@@ -40,62 +57,81 @@ func main() {
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	// log.Println("Gets all books")
+	var book Book
+	books = []Book{}
+
 	w.Header().Set("Content-Type", "application/json")
+
+	rows, err := db.Query("select id, title, author, year from books")
+	logFatal(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+		logFatal(err)
+
+		books = append(books, book)
+	}
+
 	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
-	// log.Println("Gets one book")
-	w.Header().Set("Content-Type", "application/json")
+	var book Book
 	params := mux.Vars(r)
-	// log.Println(params)
-	id, _ := strconv.Atoi(params["id"])
-	for _, book := range books {
-		if book.ID == id {
-			json.NewEncoder(w).Encode(&book)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Book{})
+
+	w.Header().Set("Content-Type", "application/json")
+
+	rows := db.QueryRow("select id, title, author, year from books where id = $1", params["id"])
+	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(book)
+
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
-	// log.Println("Adds one book")
 	var book Book
+	var bookID int
+
+	w.Header().Set("Content-Type", "application/json")
 
 	json.NewDecoder(r.Body).Decode(&book)
-	books = append(books, book)
 
-	json.NewEncoder(w).Encode(books)
+	err := db.QueryRow("insert into books (title, author, year) values($1, $2, $3) returning id;",
+		book.Title, book.Author, book.Year).Scan(&bookID)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(bookID)
 
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	// log.Println("Updates a book")
 	var book Book
 
+	w.Header().Set("Content-Type", "application/json")
 	json.NewDecoder(r.Body).Decode(&book)
-	for i, item := range books {
-		if item.ID == book.ID {
-			books[i] = book
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
 
+	res, err := db.Exec("update books set title=$1, author=$2, year=$3 where id=$4 returning id;",
+		&book.Title, &book.Author, &book.Year, &book.ID)
+	logFatal(err)
+
+	rowsUpdated, err := res.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsUpdated)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
-	// log.Println("Removes a book")
 	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-	for i, item := range books {
-		if item.ID == id {
-			books = append(books[:i], books[i+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
 
+	w.Header().Set("Content-Type", "application/json")
+	res, err := db.Exec("delete from books where id = $1", params["id"])
+	logFatal(err)
+
+	rowsDeleted, err := res.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsDeleted)
 }
